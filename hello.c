351ddfc8f5c21a63b42a814f0873a66c3bccc9e4
tdrahota2 <42596864+tdrahota2@users.noreply.github.com>
2018-08-23 09:55:36 -0500

    Added hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Taylor Drahota
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Taylor!!!\n");
+
+  return 0;
+}
